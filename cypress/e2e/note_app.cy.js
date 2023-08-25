57ba7817c6249c5292ee14aa9b93a1cfc35e45d9
larcondo <larcondo90@gmail.com>
2023-08-25 15:06:43 -0300

    install cypress library and add initial tests

---
+++ cypress/e2e/note_app.cy.js
@@ -0,0 +1,23 @@
+describe('Note app', function() {
+  beforeEach(function() {
+    cy.visit('http://localhost:5173/')
+  })
+
+  it('front page can be opened', function() {
+    cy.contains('Notes')
+    cy.contains('Note app, Department of Computer Science, University of Helsinki 2023')
+  })
+
+  it('login form can be oppened', function() {
+    cy.contains('log in').click()
+  })
+  
+  it('user can login', function() {
+    cy.contains('log in').click()
+    cy.get('#username').type('mluukkai')
+    cy.get('#password').type('salainen')
+    cy.get('#login-button').click()
+
+    cy.contains('Matti logged in')
+  })
+})
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -13,13 +13,13 @@ const LoginForm = ({
       <form onSubmit={handleSubmit}>
         <div>
           username
-          <input type="text" value={username} onChange={handleUsernameChange} />
+          <input type='text' id='username' value={username} onChange={handleUsernameChange} />
         </div>
         <div>
           password
-          <input type="password" value={password} onChange={handlePasswordChange} />
+          <input type='password' id='password' value={password} onChange={handlePasswordChange} />
         </div>
-        <button type="submit">login</button>
+        <button id='login-button' type='submit'>login</button>
       </form>
     </div>
   )
